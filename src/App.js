5b6e772143e14f5e4664fab65203933cc3ea0722
Renato Pavlic <renato.poslovni@gmail.com>
2021-08-24 23:13:01 +0200

    first commit: snap scroll and basic template

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <section className="one">
+        <h2>Section 1</h2>
+      </section>
+      <section className="two">
+        <h2>Section 2</h2>
+      </section>
+      <section className="three">
+        <h2>Section 3</h2>
+      </section>
+      <section className="four">
+        <h2>Section 4</h2>
+      </section>
+    </div>
+  );
+}
+
+export default App;
